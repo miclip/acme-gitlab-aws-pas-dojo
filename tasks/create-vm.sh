3ab28a6baaf86a6f4682dfb77d340b2ca3f94eeb
Michael Lipscombe <mlipscombe@outlook.com>
2019-04-01 15:04:10 -0600

    Save downloaded opsman

---
+++ tasks/create-vm.sh
@@ -0,0 +1,34 @@
+cat /var/version && echo ""
+set -eux
+
+mkdir generated-state
+
+vars_files_args=("")
+for vf in ${VARS_FILES}
+do
+    vars_files_args+=("--vars-file ${vf}")
+done
+
+generated_state_path="generated-state/$(basename "$STATE_FILE")"
+if [ -e "state/$STATE_FILE" ]; then
+    cp "state/$STATE_FILE" "$generated_state_path"
+fi
+
+export IMAGE_FILE
+IMAGE_FILE="$(find image/*.{yml,ova,raw} 2>/dev/null | head -n1)"
+
+if [ -z "$IMAGE_FILE" ]; then
+    echo "No image file found in image input."
+    echo "Contents of image input:"
+    ls -al image
+    exit 1
+fi
+
+# ${vars_files_args[@] needs to be globbed to split properly
+# shellcheck disable=SC2068
+p-automator create-vm \
+--config config/"${OPSMAN_CONFIG_FILE}" \
+--image-file "${IMAGE_FILE}"  \
+--state-file "$generated_state_path" \
+${vars_files_args[@]}
+# code_snippet create-vm end
