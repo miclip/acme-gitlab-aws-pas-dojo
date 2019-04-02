6c5e2af97ade9f1a1f1d06e622a57d3e8b1684de
Michael Lipscombe <mlipscombe@outlook.com>
2019-04-02 16:59:47 -0600

    Simplified upload and stage

--- tasks/upload-stage-product.sh
+++ tasks/upload-stage-product.sh
@@ -1,13 +1,8 @@
 cat /var/version && echo ""
 set -eux
 
-if [ -z "$CONFIG_FILE" ]; then
-    om --env env/"${ENV_FILE}" upload-product \
-        --product $PRODUCT_FILES/*.pivotal
-else
-    om --env env/"${ENV_FILE}" upload-product \
-        --product $PRODUCT_FILES/*.pivotal --config "config/$CONFIG_FILE"
-fi
+om --env env/"${ENV_FILE}" upload-product \
+    --product $PRODUCT_FILES/*.pivotal
 
 product_name="$(om tile-metadata \
     --product-path ${PRODUCT_FILES}/*.pivotal \
