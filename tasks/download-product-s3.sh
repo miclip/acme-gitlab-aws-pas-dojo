48400a6d260b31212bf2852911fb9bfd191fadb4
Michael Lipscombe <mlipscombe@outlook.com>
2019-04-02 13:40:17 -0600

    ...

--- tasks/download-product-s3.sh
+++ tasks/download-product-s3.sh
@@ -3,4 +3,4 @@
 
 set -eux
 
-aws s3 --endpoint-url $S3_ENDPOINT --region $S3_REGION cp "s3://${S3_BUCKET_TERRAFORM}/artifacts/${PRODUCT_NAME}" $PRODUCT_NAME --recursive
+aws s3 --endpoint-url $S3_ENDPOINT --region $S3_REGION cp "s3://${S3_BUCKET_TERRAFORM}/artifacts/${PRODUCT_NAME}" $PRODUCT_NAME --recursive --quiet
--- tasks/upload-product-s3.sh
+++ tasks/upload-product-s3.sh
@@ -3,5 +3,7 @@
 
 set -eux
 
-aws s3 --endpoint-url $S3_ENDPOINT --region $S3_REGION cp downloaded-files "s3://${S3_BUCKET_TERRAFORM}/artifacts/${PRODUCT_NAME}/downloaded-files/" --recursive
-aws s3 --endpoint-url $S3_ENDPOINT --region $S3_REGION cp assign-stemcell-config "s3://${S3_BUCKET_TERRAFORM}/artifacts/${PRODUCT_NAME}/assign-stemcell-config/" --recursive
+aws s3 --endpoint-url $S3_ENDPOINT --region $S3_REGION cp downloaded-files \
+    "s3://${S3_BUCKET_TERRAFORM}/artifacts/${PRODUCT_NAME}/downloaded-files/" --recursive --quiet
+aws s3 --endpoint-url $S3_ENDPOINT --region $S3_REGION cp assign-stemcell-config \
+        "s3://${S3_BUCKET_TERRAFORM}/artifacts/${PRODUCT_NAME}/assign-stemcell-config/" --recursive --quiet
