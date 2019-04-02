413236b2127b2fca779d792279560422b7443c1a
Michael Lipscombe <mlipscombe@outlook.com>
2019-04-02 12:03:20 -0600

    files

--- tasks/upload-product-s3.sh
+++ tasks/upload-product-s3.sh
@@ -3,6 +3,5 @@
 
 set -eux
 
-aws s3 --endpoint-url $S3_ENDPOINT --region $S3_REGION cp downloaded-product "s3://${S3_BUCKET_TERRAFORM}/artifacts/${PRODUCT_NAME}/downloaded-product/" --recursive
-aws s3 --endpoint-url $S3_ENDPOINT --region $S3_REGION cp downloaded-stemcell "s3://${S3_BUCKET_TERRAFORM}/artifacts/${PRODUCT_NAME}/downloaded-stemcell/" --recursive
+aws s3 --endpoint-url $S3_ENDPOINT --region $S3_REGION cp downloaded-files "s3://${S3_BUCKET_TERRAFORM}/artifacts/${PRODUCT_NAME}/downloaded-files/" --recursive
 aws s3 --endpoint-url $S3_ENDPOINT --region $S3_REGION cp assign-stemcell-config "s3://${S3_BUCKET_TERRAFORM}/artifacts/${PRODUCT_NAME}/assign-stemcell-config/" --recursive
