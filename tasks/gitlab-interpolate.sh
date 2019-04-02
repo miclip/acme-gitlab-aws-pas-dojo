34aa2f3f3f899efc0b8f01ab8dbf173aaf45b182
Michael Lipscombe <mlipscombe@outlook.com>
2019-04-02 10:35:55 -0600

    config dir

--- tasks/gitlab-interpolate.sh
+++ tasks/gitlab-interpolate.sh
@@ -2,20 +2,24 @@
 
 set -eux
 
-if [ -z "$VAR_PREFIX" ]; then
-    echo "Please specify a VAR_PREFIX. It is required."
-    exit 1
-fi
+for FILE_GLOB in $FILES_GLOB; do 
 
-if [ ! -d "interpolated-files" ]; then 
-     mkdir interpolated-files
-fi 
+    if [ -z "$VAR_PREFIX" ]; then
+        echo "Please specify a VAR_PREFIX. It is required."
+        exit 1
+    fi
 
-for file in $FILES_GLOB; do
-    echo "interpolating $file"
-    om interpolate -c "$file" --vars-env "$VAR_PREFIX" > interpolated-files/"$(basename "$file")"
+    if [ ! -d "interpolated-files" ]; then 
+        mkdir interpolated-files
+    fi 
+
+    for file in $FILE_GLOB; do
+        echo "interpolating $file"
+        om interpolate -c "$file" --vars-env "$VAR_PREFIX" > interpolated-files/"$(basename "$file")"
+    done
+
+    DIR=$(dirname "${FILE_GLOB}")
+    cp -R ./interpolated-files/*.* "${DIR}/"
+    rm -rf ./interpolated-files
+
 done
-
-DIR=$(dirname "${FILES_GLOB}")
-cp -R ./interpolated-files/*.* "${DIR}/"
-rm -rf ./interpolated-files
