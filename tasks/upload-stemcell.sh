71b213b46d005ec577deb7186196873c6578f0d4
Michael Lipscombe <mlipscombe@outlook.com>
2019-04-03 09:13:02 -0600

    Fix floating stemcell

--- tasks/upload-stemcell.sh
+++ tasks/upload-stemcell.sh
@@ -1,6 +1,6 @@
 cat /var/version && echo ""
 set -eux
-FLOATING_STEMCELL=""
+FLOATING_STEMCELL=true
 om --env env/"${ENV_FILE}" upload-stemcell \
     --floating="$FLOATING_STEMCELL" \
     --stemcell $STEMCELL_DIR/*.tgz
