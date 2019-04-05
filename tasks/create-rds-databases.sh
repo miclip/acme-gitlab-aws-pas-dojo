7df0a56be2664dd75996ed0ad0dbf6473488d9e3
Michael Lipscombe <mlipscombe@outlook.com>
2019-04-05 14:16:56 -0600

    Change RDS vars

--- tasks/create-rds-databases.sh
+++ tasks/create-rds-databases.sh
@@ -28,7 +28,7 @@ routing
 silk
 uaa"
      for i in \$dbs; do
-         mysql -h ${rds_address} -u admin -p${rds_password}  -e "CREATE DATABASE \$i;"
+         mysql -h ${rds_instance_endpoint} -u admin -p${rds_instance_password}  -e "CREATE DATABASE \$i;"
      done;
 EOF
 }
